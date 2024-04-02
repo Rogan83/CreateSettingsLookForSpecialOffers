3f65bd53bb56a1efd3864fc205a971b59c44026a
Daniel Rothweiler <D.Rothweiler@yahoo.de>
2024-04-02 19:19:08 +0200

    Löschen implementiert

---
+++ MVVM/Models/FavoriteProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSettingsLookForSpecialOffers.MVVM.Models
+{
+    internal class FavoriteProduct
+    {
+        public string Name { get; set; }
+        public decimal PriceCapPerProduct { get; set; }
+        public decimal PriceCapPerKg { get; set; }
+
+        public FavoriteProduct(string name, decimal priceCapPerKg, decimal priceCapPerProduct = 0)
+        {
+            Name = name;
+            PriceCapPerProduct = priceCapPerProduct;
+            PriceCapPerKg = priceCapPerKg;
+        }
+    }
+}
--- MVVM/ViewModels/SettingsModel.cs
+++ MVVM/ViewModels/SettingsModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CreateSettingsLookForSpecialOffers.MVVM.Models;
+using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,12 +10,38 @@ using System.Windows.Input;
 
 namespace CreateSettingsLookForSpecialOffers.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     internal class SettingsModel
     {
-        public ICommand Test =>
-            new Command(async () =>
+        public ObservableCollection<FavoriteProduct> Products { get; set; } = new ObservableCollection<FavoriteProduct>{
+            new FavoriteProduct("Speisequark", 2.60m),
+            new FavoriteProduct("Thunfisch", 5.08m),
+            new FavoriteProduct("Tomate", 2.00m),
+            new FavoriteProduct("Orange", 0.99m),
+            new FavoriteProduct("Buttermilch", 0.99m),
+            new FavoriteProduct("Äpfel", 1.99m),
+            new FavoriteProduct("Hackfleisch", 5.99m)
+        };
+
+
+        public ICommand AddFavoriteProduct =>
+            new Command(() =>
             {
-                
+                Products.Add(new FavoriteProduct("quark", 2.99m));
+            });
+
+        public ICommand DeleteAllFavoriteProducts =>
+            new Command(() =>
+            {
+                Products.Clear();
+            });
+
+        public ICommand DeleteFavoriteProduct =>
+            new Command((item) =>
+            {
+                FavoriteProduct product = (FavoriteProduct)item;
+                if (product != null)
+                    Products.Remove(product);
             });
     }
 }
